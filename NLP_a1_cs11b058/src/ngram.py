b959f370dd1c02b37d239252ec1f1b397800ce00
Sriram V <sriram.vasudevan.93@gmail.com>
2014-10-26 13:20:30 +0530

    Project report updated

---
+++ NLP_a1_cs11b058/src/ngram.py
@@ -0,0 +1,230 @@
+import re, math, sys, time, itertools
+import word_check
+
+
+MIN_TRIGRAM_PROB = math.pow(10,(-7))
+MIN_UNIGRAM_PROB = math.pow(10,(-8))
+CONFUSION_SET_SIZE = 20
+
+
+def ngrams(array, n):
+    return [array[i:i+n] for i in range(1+len(array)-n)]
+
+
+def print_words_from_phrase(query, sentences, num_misspelt_words):
+    if num_misspelt_words == 0:
+        print query, '\t',
+        print sentences[0], '\t', sentences[1]
+    elif num_misspelt_words == 1:
+        if sentences:
+            words = extract_words(query)
+            print words[sentences[0][-1]], '\t',
+            count = 0
+            suggestions = {}
+            for sentence in sentences:
+                if sentence[-2] not in suggestions:
+                    suggestions[sentence[-2]] = sentence[1]
+                    print sentence[-2], "\t", sentence[1],
+                    count += 1
+                    if count == 5:
+                        break
+            print ''
+        else:
+            print query
+    else:
+        words = extract_words(query)
+        for pos in sentences[0][-1]:
+            print words[pos], '\t',
+            count = 0
+            suggestions = {}
+            for sentence in sentences:
+                if sentence[0][pos] not in suggestions:
+                    suggestions[sentence[0][pos]] = sentence[1]
+                    print sentence[0][pos], "\t", sentence[1],
+                    count += 1
+                    if count == 5:
+                        break
+            print ''
+
+
+def print_sentences_from_list(query, sentences):
+    print query, "\t",
+    for sentence in sentences:
+        for word in sentence[0]:
+            print word,
+        print "\t",sentence[1],"\t",
+    print ''
+    #print query
+    #for sentence in sentences:
+    #    for word in sentence[0]:
+    #        print word,
+    #    print " ", sentence[1]
+    #print ''
+
+
+def all_grams(array,word_pos):
+    l = []
+    for i in range(2,6):
+        for j in range(word_pos-i+1,word_pos+1):
+            
+            if j>=0 and j+i <= len(array):
+                l.append(array[j:j+i])
+    return l
+
+
+def extract_words(text):
+    return re.findall('[a-z]+', text.lower()) 
+
+
+def read_ngram_counts(n):
+    index = {}
+    file_name = 'data/w'+str(n)+'_.txt'
+    with open(file_name) as f:
+        lines = f.read().splitlines()
+        for line in lines:
+            split_line = line.split('\t')    
+            freq = int(split_line[0])
+            words = []
+            for x in split_line[1:]:
+                words.append(x.lower())
+            word_list = tuple(words)
+            index[word_list] = freq
+    return index
+
+
+def estimate_ngram_probabilities(ngram_count_index,n_minus1_gram_count_index,n):
+    ngram_prob_index = dict(ngram_count_index)
+    for ngram in ngram_count_index:
+        if n >2:
+            n_minus1_gram = ngram[0:n-1]
+        else:
+            n_minus1_gram = ngram[0]
+
+        if n_minus1_gram not in n_minus1_gram_count_index:
+            # Ignore n gram if n-1 gram doesn't exist
+            #print "del ",n_minus1_gram
+            del ngram_prob_index[ngram]
+        else:
+            ngram_prob_index[ngram] = ngram_prob_index[ngram]/float(n_minus1_gram_count_index[n_minus1_gram])
+    return ngram_prob_index
+
+
+def find_prob_sentence_all_grams(sentence,word_pos,fivegram_count_index,quadgram_count_index,trigram_count_index,bigram_count_index):
+    count = 0
+    ngrams_sentence = all_grams(sentence,word_pos)
+    for ngram in ngrams_sentence:
+        if len(ngram) ==5 and tuple(ngram) in fivegram_count_index:
+            count += math.log(fivegram_count_index[tuple(ngram)] *1)
+        elif len(ngram) ==4 and tuple(ngram) in quadgram_count_index:
+            count += math.log(quadgram_count_index[tuple(ngram)] *1)
+        elif len(ngram) ==3 and tuple(ngram) in trigram_count_index:
+            count += math.log(trigram_count_index[tuple(ngram)] *1)
+        elif len(ngram) ==2 and tuple(ngram) in bigram_count_index:
+            count += math.log(bigram_count_index[tuple(ngram)] *1)
+    return count
+
+
+def compute_scores(phrase,preprocessed):
+    (prior_frequencies,ngram_words,matrices,dictionary,phonetic) = preprocessed
+    #don't use phonetic here
+    words = extract_words(phrase)
+    pos = 0
+    num_misspelt_words =0
+    phrase_results = []
+    for word in words:
+        results = []
+        # Search in UNIX dictionary.
+        if word not in dictionary:
+            num_misspelt_words    += 1
+            confusion_set =  word_check.get_confusion_set(word,prior_frequencies,ngram_words,matrices,CONFUSION_SET_SIZE)    
+            max_score = 0
+            max_score_1 = 0
+            max_sentence = []
+            max_likelihood = -100000
+            for confused_triple in confusion_set:
+                confused_word = confused_triple[0]
+                edit_dist = confused_triple[1]
+                likelihood = math.log(confused_triple[2]) #note: computation includes prior too
+                sentence  = list(words)
+                sentence[pos] = confused_word
+                score1 = find_prob_sentence_all_grams(sentence,pos,fivegram_count_index,quadgram_count_index,trigram_count_index,bigram_count_index)
+                
+                if score1 > max_score_1:
+                    max_score_1 = score1
+                if likelihood > max_likelihood:
+                    max_likelihood = likelihood
+                results.append((sentence,score1,likelihood,confused_word,pos))
+
+            results_new = []
+            for res in results:
+                if max_score_1 == 0:
+                    a = max_score_1
+                else:
+                    a = res[1]/max_score_1
+                b = res[2]/abs(max_likelihood)
+                results_new.append((res[0],(0.7*a+0.3*b),a,b,res[3],res[4]))
+            phrase_results.append(results_new)
+        pos +=1
+
+    if num_misspelt_words ==0 :
+        #print_words_from_phrase(phrase, [phrase, 0.0], num_misspelt_words)
+        print_sentences_from_list(phrase, [[[phrase], 0.0]])
+    elif num_misspelt_words == 1 and len(phrase_results) >0:
+        #print_words_from_phrase(phrase, sorted(phrase_results[0],key=lambda x: x[1],reverse=True), num_misspelt_words)
+        print_sentences_from_list(phrase, sorted(phrase_results[0],key=lambda x: x[1],reverse=True)[0:5])
+    elif len(phrase_results) >0:
+        phrase_results = [sorted(p,key=lambda x: x[1],reverse=True)[:3] for p in phrase_results]
+        combos = list(itertools.product(*phrase_results))
+        results_new = []
+        max_score = [0 for i in range(0, num_misspelt_words)]
+        #pos_list = [x[-1] for x in combos[0]]
+        for c in combos:
+            sentence = [x for x in c[0][0]]
+            for i in range(1,num_misspelt_words):
+                sentence[c[i][5]] = c[i][4]
+            temp_list = [sentence]
+            for i in range(0,num_misspelt_words):
+                score = find_prob_sentence_all_grams(sentence,c[i][5],fivegram_count_index,quadgram_count_index,trigram_count_index,bigram_count_index)
+                max_score[i] = max(score, max_score[i])
+                temp_list.append(score)
+                temp_list.append(c[i][3])
+            results_new.append((temp_list))
+        results_updated = []
+        for res in results_new:
+            netval = 0
+            for i in range(0,num_misspelt_words):
+                netval += (0.7*res[2*i+1]/max_score[i])+0.3*res[2*i+2]
+            #results_updated.append((res[0], netval, pos_list))
+            results_updated.append((res[0], netval))
+        #print_words_from_phrase(phrase, sorted(results_updated,key=lambda x: x[1],reverse=True), num_misspelt_words)
+        print_sentences_from_list(phrase, sorted(results_updated,key=lambda x: x[1],reverse=True)[0:5])
+
+
+def run_test_data(trigram_prob_index,fivegram_count_index,quadgram_count_index,trigram_count_index,bigram_count_index):
+    preprocessed = word_check.preprocessing()
+    with open('../TrainData/sentences.tsv') as f:
+        lines = f.read().splitlines()
+        for line in lines:
+            phrase = line.split('\t')[0]
+            #start_time = time.time()
+            compute_scores(phrase,preprocessed)
+            #print time.time()-start_time
+
+
+def run_input(trigram_prob_index,fivegram_count_index,quadgram_count_index,trigram_count_index,bigram_count_index):
+    preprocessed = word_check.preprocessing()
+    while True:
+        phrase = raw_input('Enter a phrase  (# to stop) : ')
+        if phrase == '#':
+            sys.exit(0)
+        compute_scores(phrase,preprocessed)
+
+
+if __name__ == '__main__':
+    bigram_count_index = read_ngram_counts(2)
+    trigram_count_index = read_ngram_counts(3)
+    quadgram_count_index = read_ngram_counts(4)
+    fivegram_count_index = read_ngram_counts(5)
+    trigram_prob_index = estimate_ngram_probabilities(trigram_count_index,bigram_count_index,3)
+    run_test_data(trigram_prob_index,fivegram_count_index,quadgram_count_index,trigram_count_index,bigram_count_index)
+#run_input(trigram_prob_index,fivegram_count_index,quadgram_count_index,trigram_count_index,bigram_count_index)
---
+++ NLP_a1_cs11b058/src/word_check.py
@@ -0,0 +1,279 @@
+from sets import Set
+import time, re, operator, metaphone
+MAX_EDIT = 3
+NGRAM_N = 2
+
+
+class TrieNode:
+    def __init__(self):
+        self.word = None
+        self.children = {}
+
+    def insert( self, word ):
+        node = self
+        for letter in word:
+            if letter not in node.children: 
+                node.children[letter] = TrieNode()
+            node = node.children[letter]
+        node.word = word
+
+
+def search(word, matrices, trie):
+    # build first row
+    currentProb = [1.0]
+    for j in range(len(word)):
+        if j:
+            newprob = matrices[0][26][ord(word[j])-97]/matrices[5][26] + matrices[0][ord(word[j-1])-97][ord(word[j])-97]/matrices[5][ord(word[j-1])-97]
+        else:
+            newprob = matrices[0][26][ord(word[j])-97]/matrices[5][26]
+        currentProb.append(currentProb[j]*newprob)
+    currentRow = range(len(word)+1)
+    columns = len(word)
+    curr_index = 0
+    results = []
+    # recursively search each branch of the trie
+    for letter in trie.children:
+        searchRecursive(trie.children[letter], letter, word, None, None, currentRow, currentProb, results, curr_index, columns, matrices)
+    return results
+
+
+# This recursive helper is used by the search function above. It assumes that
+# the previousRow has been filled in already. - Trie code
+def searchRecursive(node, w1, w2, twoago, twoagoProb, previousRow, prevProb, results, i, columns, matrices):
+    if i:
+        newprob = matrices[2][ord(w1[i-1])-97][ord(w1[i])-97]/matrices[3][ord(w1[i-1])-97][ord(w1[i])-97] + matrices[2][26][ord(w1[i])-97]/matrices[3][26][ord(w1[i])-97]
+    else:
+        newprob = matrices[2][26][ord(w1[0])-97]/matrices[3][26][ord(w1[0])-97]
+    currentProb = [prevProb[0]*newprob]
+    currentRow = [previousRow[0] + 1]
+
+    # Build one row for the letter, with a column for each letter in the target
+    # word, plus one for the empty string at column 0
+    for j in xrange(columns):
+        deleteCost = previousRow[j+1] + 1
+        insertCost = currentRow[j] + 1
+        replaceCost = previousRow[j] + (w1[i] != w2[j])
+
+        minval = deleteCost
+        minlist = ['d']
+        for val in [(insertCost, 'i'), (replaceCost, 's')]:
+            if val[0]<minval:
+                minval = val[0]
+                minlist = [val[1]]
+            elif val[0]==minval:
+                minlist.append(val[1])
+
+        # This block deals with transpositions
+        if (i and j and w1[i] == w2[j-1] and w1[i-1] == w2[j]):
+            transposeCost = twoago[j-1] + (w1[i] != w2[j])
+            minval = min(minval, transposeCost)
+            if transposeCost<minval:
+                minval = transposeCost
+                minlist = ['t']
+            elif transposeCost==minval:
+                minlist.append('t')
+
+        newprob = 0.0
+        for elem in minlist:
+            if elem == 's':
+                if w1[i]!=w2[j]:
+                    if previousRow[j]: # edit dist of prev is non-zero
+                        newprob += prevProb[j]*2*matrices[1][ord(w2[j])-97][ord(w1[i])-97]/matrices[5][ord(w1[i])-97]
+                    else:
+                        newprob += prevProb[j]*matrices[1][ord(w2[j])-97][ord(w1[i])-97]/matrices[5][ord(w1[i])-97]
+                else:
+                    newprob += prevProb[j]
+            elif elem == 't':
+                if w1[i]!=w2[j]:
+                    if twoago[j-1]: # edit dist of prev is non-zero
+                        newprob += twoagoProb[j-1]*2*matrices[4][ord(w1[i-1])-97][ord(w1[i])-97]/matrices[3][ord(w1[i-1])-97][ord(w1[i])-97]
+                    else:
+                        newprob += twoagoProb[j-1]*matrices[4][ord(w1[i-1])-97][ord(w1[i])-97]/matrices[3][ord(w1[i-1])-97][ord(w1[i])-97]
+                else:
+                    newprob += twoagoProb[j-1]
+            elif elem == 'i':
+                if currentRow[j]:
+                    newprob += currentProb[j]*(matrices[0][ord(w1[i])-97][ord(w2[j])-97]/matrices[5][ord(w1[i])-97]+matrices[0][ord(w2[j-1])-97][ord(w2[j])-97]/matrices[5][ord(w2[j-1])-97])
+                else:
+                    newprob += currentProb[j]*matrices[0][ord(w1[i])-97][ord(w2[j])-97]/matrices[5][ord(w1[i])-97]
+            elif elem == 'd':
+                if previousRow[j+1]:
+                    newprob += prevProb[j+1]*(matrices[2][ord(w1[i-1])-97][ord(w1[i])-97]/matrices[3][ord(w1[i-1])-97][ord(w1[i])-97]+matrices[2][ord(w2[j])-97][ord(w1[i])-97]/matrices[3][ord(w2[j])-97][ord(w1[i])-97])
+                else:
+                    newprob += prevProb[j+1]*matrices[2][ord(w1[i-1])-97][ord(w1[i])-97]/matrices[3][ord(w1[i-1])-97][ord(w1[i])-97]
+
+        currentProb.append(newprob)
+        currentRow.append(minval)
+
+    # if the last entry in the row indicates the optimal cost is less than the
+    # maximum cost, and there is a word in this trie node, then add it.
+    if currentRow[columns] <= MAX_EDIT and node.word != None:
+        results.append((node.word, currentRow[columns], currentProb[columns]))
+
+    # if any entries in the row are less than the maximum cost, then 
+    # recursively search each branch of the trie
+    if min(currentRow) <= MAX_EDIT:
+        for letter in node.children:
+            searchRecursive(node.children[letter], w1+letter, w2, previousRow, prevProb, currentRow, currentProb, results, i+1, columns, matrices)
+
+
+'''
+Returns list of ngrams for a word
+'''
+def ngrams(word, n):
+    return [word[i:i+n] for i in range(1+len(word)-n)]
+
+
+'''
+Returns index structure indexed by ngrams and has an entry which is list of words that have the ngram
+'''
+def ngram_index_structure(words,n):
+    alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+    index = {x+y:{} for x in alphabet for y in alphabet}
+    for word in words:
+        ngrams_list = ngrams(word,n)
+        for ngram in ngrams_list:
+            if len(word) not in index[ngram]:
+                index[ngram][len(word)] = []
+            index[ngram][len(word)].append(word)
+    return index
+
+
+'''
+Returns a set of candidate words using the ngram index structure created
+'''
+def similarity_prune(ngram_words, word, n):
+    intersect_dict = {}
+    lb = max(len(word)-MAX_EDIT,2)
+    ub = len(word)+MAX_EDIT+1
+    for ngram in ngrams(word,n):
+        for length in range(lb, ub):
+            if length in ngram_words[ngram]:
+                for word in ngram_words[ngram][length]:
+                    if word not in intersect_dict:
+                        intersect_dict[word] = 0
+                    intersect_dict[word] += 1
+    
+    # generate similarity scores
+    intersect_dict = {k:v/float(len(k)-n+1) for k, v in intersect_dict.iteritems()} # denominator is no. of ngrams
+    sorted_x = sorted(intersect_dict.items(), key=operator.itemgetter(1), reverse= True)
+    final =  [x[0] for x in sorted_x[:300]]
+    return final
+
+
+def preprocessing():
+    words = []
+    ngram_words = {}
+    prior_frequencies = {}
+    total_frequencies = 0
+    matrices = []
+    phonetic = {}
+
+    # Reading dictionary
+    with open('data/unixdict.txt') as f:
+        for line in f.read().splitlines():
+            word = line.split('\t')[0]
+            words.append(word)
+            phonetic[word] = metaphone.dm(word)
+            prior_frequencies[word] = 1 # Doing add one
+
+    #Reading priors        
+    with open('data/count_1w.txt') as f:
+       for line in f.read().splitlines():
+           word = line.split('\t')[0]
+           freq = line.split('\t')[1]
+           if word in prior_frequencies:
+               prior_frequencies[word] = int(freq)
+               total_frequencies += int(freq)
+
+    # Divide by total frequency to get probability
+    prior_frequencies = {k:v/float(total_frequencies) for k, v in prior_frequencies.iteritems()}
+    ngram_words =  ngram_index_structure(words,NGRAM_N)
+
+    # Load matrices
+    files = ['data/addoneAddXY.txt', 'data/addoneSubXY.txt', 'data/addoneDelXY.txt', 'data/newCharsXY.txt', 'data/addoneRevXY.txt', 'data/sumnewCharsXY.txt']
+    for f in files[:-1]:
+        matrix = []
+        for lines in file(f).readlines():
+            matrix.append([float(x) for x in lines.split()])
+        matrices.append(matrix)
+    # Last one is a vector, not a matrix
+    matrix = []
+    for lines in file(files[-1]).readlines():
+        matrix.append(float(lines))
+    matrices.append(matrix)
+
+    return (prior_frequencies,ngram_words,matrices,words,phonetic)
+
+
+def get_confusion_set(misspelt_word,prior_frequencies,ngram_words,matrices,n):
+    candidate_selections = similarity_prune(ngram_words, misspelt_word, NGRAM_N)
+
+    trie = TrieNode()
+    for word in candidate_selections:
+        trie.insert(word)
+
+    results = search(misspelt_word, matrices,trie)
+    results = [(x[0],x[1],x[2]) for x in results]
+    results.sort(key=lambda x: x[2], reverse=True)
+    return results[0:n]
+
+
+def phonetic_score(w1, w2):
+    if w1[0]==w2[0]:
+        return 20
+    elif w1[0]==w2[1] or w1[1]==w2[0]:
+        return 1
+    else:
+        return 0.05
+
+
+def print_words_from_list(query, suggestions):
+    print query,"\t",
+    for suggestion in suggestions:
+        print suggestion[0],"\t",suggestion[2],"\t",
+    print ''
+    #print query
+    #for suggestion in suggestions:
+    #    print suggestion
+    #print ''
+
+
+def get_results(misspelt_word,prior_frequencies,ngram_words,matrices,phonetic):
+    #start_time = time.time()
+    candidate_selections = similarity_prune(ngram_words, misspelt_word, NGRAM_N)
+    word_ph = metaphone.dm(misspelt_word)
+
+    trie = TrieNode()
+    for word in candidate_selections:
+        trie.insert(word)
+
+    results = search(misspelt_word, matrices,trie)
+    results = [(x[0],x[1],x[2]*prior_frequencies[x[0]]*phonetic_score(word_ph, phonetic[x[0]])) for x in results]
+    print_words_from_list(misspelt_word, sorted(results, key=lambda x: x[2], reverse=True)[:5])
+    #print time.time()-start_time
+
+
+def run_test_data(prior_frequencies,ngram_words,matrices,phonetic):
+    #start_time = time.time()
+    with open('../TrainData/words.tsv') as f:
+        lines = f.read().splitlines()
+        for line in lines:
+            misspelt_word = line.split('\t')[0]
+            get_results(misspelt_word,prior_frequencies,ngram_words,matrices,phonetic)
+    #print 'time=', time.time()-start_time
+
+
+def run_input(prior_frequencies,ngram_words,matrices,phonetic):
+    while True:
+        misspelt_word = raw_input('Enter a word (# to stop) : ')
+        if misspelt_word == '#' or misspelt_word == "#":
+            break
+        get_results(misspelt_word,prior_frequencies,ngram_words,matrices,phonetic)
+
+
+if __name__=='__main__':
+    (prior_frequencies,ngram_words,matrices,dictionary,phonetic) = preprocessing()
+    run_test_data(prior_frequencies,ngram_words,matrices,phonetic)
+    #run_input(prior_frequencies,ngram_words,matrices,phonetic)
+    #print get_confusion_set('eath',prior_frequencies,ngram_words,matrices,2)
