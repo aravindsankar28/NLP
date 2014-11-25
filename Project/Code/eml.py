cd6a4a51560dde1296721e8e37a50679443f7c02
Sriram V <sriram.vasudevan.93@gmail.com>
2014-11-26 01:42:16 +0530

    Adding Mails and Subjects

--- Project/Code/eml.py
+++ Project/Code/eml.py
@@ -18,7 +18,7 @@ for part in msg.walk():
     # each part is a either non-multipart, or another multipart message
     # that contains further parts... Message is organized like a tree
     if part.get_content_type() == 'text/plain':
-        mail.write(part.get_payload()) # prints the raw te
+        mail.write(part.get_payload()) # prints the raw text
 
 
 sub.close()
